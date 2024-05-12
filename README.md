13510f908677a381dd40d67349ad39cbd7fc6380
AlOlmo <berto.olmo@gmail.com>
2024-05-12 15:50:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Extractor de páginas amarillas
+
+## Crear y activar entorno
+
+To create a virtual environment, decide upon a directory where you want to place it, and run the venv module as a script with the directory path:
+
+```
+python -m venv .venv
+```
+
+Once you’ve created a virtual environment, you may activate it.
+
+On Windows, run:
+```
+.venv\Scripts\activate
+```
+
+On Unix or MacOS, run:
+```
+source .venv/bin/activate
+```
+
+## Instalar dependencias
+
+```
+pip install -r requirements.txt
+```
+
+## Arrancar el proceso
+
+```
+python Main.py [args]
+```
---
+++ paginas_amarillas_extractor.py
@@ -0,0 +1,40 @@
+import time
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup, PageElement
+import pandas as pd
+
+def safe_extract_text(element: PageElement):
+    if element is not None:
+        return element.text
+    else:
+        return ''
+
+if __name__ == '__main__':
+
+    for number in range(1, 200):
+        time_1 = time.process_time()
+        # Request
+        html = requests.get(
+            url=f"https://www.paginasamarillas.es/search/jardineros/all-ma/all-pr/all-is/all-ci/all-ba/all-pu/all-nc/{number}?what=jardineros",
+            headers={'User-agent': 'Mozilla/5.0'}
+        ).content
+
+        # Extract data
+        soup = BeautifulSoup(html, "html.parser")
+        items = soup.find_all("div", {"class": "listado-item"})
+        parsed_items = []
+        for item in items:
+            parsed_items.append({
+                "name": safe_extract_text(item.find_next("span", {"itemprop": "name"})),
+                "postal_code": safe_extract_text(item.find_next("span", {"itemprop": "postalCode"})),
+                "province": safe_extract_text(item.find_next("span", {"itemprop": "addressRegion"})),
+                "city": safe_extract_text(item.find_next("span", {"itemprop": "addressLocality"})),
+                "phone": safe_extract_text(item.find_next("span", {"itemprop": "telephone"}))
+            })
+
+        # Write file
+        pd.DataFrame.from_dict(parsed_items).to_csv("jardineros.csv", mode='a', header=False)
+        time_2 = time.process_time()
+        print(f"Page {number} scrapped (time: {time_2 - time_1})")
